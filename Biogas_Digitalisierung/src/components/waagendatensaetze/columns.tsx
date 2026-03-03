088ac03b3a2edb08e661e51208f81ff39e9b40f7
WaffenWaffel <hannes.doppelbauer@outlook.com>
2026-03-03 23:10:41 +0100

    Prisma added

--- Biogas_Digitalisierung/src/components/waagendatensaetze/columns.tsx
+++ Biogas_Digitalisierung/src/components/waagendatensaetze/columns.tsx
@@ -1,23 +1,18 @@
 // "use client"
 
 import type { ColumnDef } from "@tanstack/react-table"
-import { Button } from "../ui/button";
-import { ArrowUpDown } from "lucide-react"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
 export type WaageDaten = {
-    WS_Nr: Number;
-    Kennzeichen1: String;
-    Kennzeichen2: String;
-    EW_Gewicht: Number;
-    ZW_Gewicht: Number;
-    Netto_Gewicht: Number;
+    Ws_Nr: number;
     Datum: String;
     Uhrzeit: String;
+    Kennzeichen1: String;
+    Kennzeichen2: String;
+    Netto_Gewicht: Number;
     Sorte: String;
-    Warenfluss: String;
     Erzeuger_Name: String;
     Schlag_ID: Number;
     Schlag_Name: String;
@@ -25,8 +20,16 @@ export type WaageDaten = {
 
 export const columns: ColumnDef<WaageDaten>[] = [
   {
-    accessorKey: "WS_Nr",
-    header: "WS_Nr",
+    accessorKey: "Ws_Nr",
+    header: "Ws_Nr",
+  },
+  {
+    accessorKey: "Datum",
+    header: "Datum",
+  },
+  {
+    accessorKey: "Uhrzeit",
+    header: "Uhrzeit",
   },
   {
     accessorKey: "Kennzeichen1",
@@ -37,42 +40,12 @@ export const columns: ColumnDef<WaageDaten>[] = [
     header: "Kennzeichen2",
   },
   {
-    accessorKey: "EW_Gewicht",
-    header: "EW_Gewicht",
-  },
-  {
-    accessorKey: "ZW_Gewicht",
-    header: "ZW_Gewicht",
-  },
-  {
     accessorKey: "Netto_Gewicht",
     header: "Netto_Gewicht",
   },
   {
-    accessorKey: "Datum",
-    header: "Datum",
-  },
-  {
-    accessorKey: "Uhrzeit",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Email
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      )
-    },
-  },
-  {
     accessorKey: "Sorte",
     header: "Sorte",
-  },
-  {
-    accessorKey: "Warenfluss",
-    header: "Warenfluss",
   },
   {
     accessorKey: "Erzeuger_Name",
